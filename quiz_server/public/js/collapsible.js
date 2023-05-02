b4327ca7b6a0318bbf37b9b5e8e22124431170f6
James Leipet <jleipert123@gmail.com>
2023-05-02 22:22:22 +0200

    more css work on favorites page

--- quiz_server/public/js/collapsible.js
+++ quiz_server/public/js/collapsible.js
@@ -1,29 +1,61 @@
-let coll = document.getElementsByClassName("collapsible");
-let i;
+const accordion = document.querySelector(".accordion");
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+accordion.addEventListener("click", (e) => {
+  const activePanel = e.target.closest(".accordion-panel");
+  if (!activePanel) return;
+  toggleAccordion(activePanel);
+});
 
-    this.classList.toggle("active");
-    let content = this.lastElementChild;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+function toggleAccordion(panelToActivate) {
+  const activeButton = panelToActivate.querySelector("button");
+  const activePanel = panelToActivate.querySelector(".accordion-content");
+  const activePanelIsOpened = activeButton.getAttribute("aria-expanded");
+
+  if (activePanelIsOpened === "true") {
+    panelToActivate
+      .querySelector("button")
+      .setAttribute("aria-expanded", false);
+
+    panelToActivate
+      .querySelector(".accordion-content")
+      .setAttribute("aria-hidden", true);
+      activeButton.classList.toggle("active");
+  } else {
+    panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
+
+    panelToActivate
+      .querySelector(".accordion-content")
+      .setAttribute("aria-hidden", false);
+      activeButton.classList.toggle("active");
+  }
 }
 
-window.onclick = function (event) {
-    if (!event.target.matches(".collapsible")) {
-        let content = document.getElementsByClassName("content");
+// let coll = document.getElementsByClassName("collapsible");
+// let i;
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+
+//     this.classList.toggle("active");
+//     let content = this.lastElementChild;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
+
+// window.onclick = function (event) {
+//     if (!event.target.matches(".collapsible")) {
+//         let content = document.getElementsByClassName("content");
         
-        for (let i = 0; i < content.length; i++) {
+//         for (let i = 0; i < content.length; i++) {
             
-            if (content[i].style.display == "block") {
-                content[i].style.display = "none";
-            }
-            coll[i].classList.remove("active");
-        }
-    }
-};
+//             if (content[i].style.display == "block") {
+//                 content[i].style.display = "none";
+//             }
+//             coll[i].classList.remove("active");
+//         }
+//     }
+// };
