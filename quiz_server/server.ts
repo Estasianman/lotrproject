11d62cd8d41047b17d4cf8de59c342823c58cc3e
James Leipert <james.leipert@student.ap.be>
2023-05-13 17:39:46 +0200

    added types.ts with all interfaces

--- quiz_server/server.ts
+++ quiz_server/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const axios = require("axios");
 import { MongoClient } from "mongodb";
+import {Player, Blacklist, FavoriteList, Characters, Doc, Gender, Quotes, qDoc, Movie, Movies, MovieDoc, RandomFunction, CombineArrayElements, ApiData, Highscores, GameVariables } from "./types";
 const uri =
   "mongodb+srv://server:server123@rikcluster.mh2n1dx.mongodb.net/?retryWrites=true&w=majority";
 const app = express();
@@ -33,126 +34,6 @@ app.use(
   })
 );
 
-// Player interface which is saved in databank
-interface Player {
-  name: string;
-  ww: string;
-  qscore?: number;
-  sdscore?: number;
-  favorites?: FavoriteList[];
-  blacklisted?: Blacklist[];
-}
-
-// Blacklist interface
-interface Blacklist {
-  characterName: string;
-  blacklistQuotes: string[];
-  reason: string[];
-}
-
-// FavoriteList interface
-interface FavoriteList {
-  characterName: string;
-  favoriteQuotes: string[];
-  characterInfo: Doc;
-}
-
-//API character interface
-export interface Characters {
-  docs: Doc[];
-  total: number;
-  limit: number;
-  offset: number;
-  page: number;
-  pages: number;
-}
-
-export interface Doc {
-  _id: string;
-  height: string;
-  race: string;
-  gender?: Gender;
-  birth: string;
-  spouse: string;
-  death: string;
-  realm: string;
-  hair: string;
-  name: string;
-  wikiUrl?: string;
-}
-
-export enum Gender {
-  Empty = "",
-  Female = "Female",
-  GenderMale = "male",
-  Male = "Male",
-  Males = "Males",
-  MostLikelyMale = "Most likely male",
-  NaN = "NaN",
-}
-
-//API quote interfaces
-export interface Quotes {
-  docs: qDoc[];
-  total: number;
-  limit: number;
-  offset: number;
-  page: number;
-  pages: number;
-}
-
-export interface qDoc {
-  _id: string;
-  dialog: string;
-  movie: Movie;
-  character: string;
-  id: string;
-}
-
-export enum Movie {
-  The5Cd95395De30Eff6Ebccde5B = "5cd95395de30eff6ebccde5b",
-  The5Cd95395De30Eff6Ebccde5D = "5cd95395de30eff6ebccde5d",
-}
-
-//API Movie interface
-export interface Movies {
-  docs: MovieDoc[];
-  total: number;
-  limit: number;
-  offset: number;
-  page: number;
-  pages: number;
-}
-
-export interface MovieDoc {
-  _id: string;
-  name: string;
-  runtimeInMinutes: number;
-  budgetInMillions: number;
-  boxOfficeRevenueInMillions: number;
-  academyAwardNominations: number;
-  academyAwardWins: number;
-  rottenTomatoesScore: number;
-}
-
-// Random generator interface
-interface RandomFunction {
-  (min: number, max: number): number;
-}
-
-interface CombineArrayElements {
-  (arrayEmpty: any[], arrayToTransfer: any[]): void;
-}
-
-// interface to save data from the API
-interface ApiData {
-  quote: qDoc;
-  charactersArray: Doc[];
-  moviesArray: MovieDoc[];
-  correctCharacterName: string;
-  correctMovieName: string;
-}
-
 let apiData: ApiData = {
   quote: {
     _id: "",
@@ -178,35 +59,6 @@ let characters: Characters = {
   page: 0,
   pages: 0,
 };
-
-// main game variables used in the quiz
-interface Highscores {
-  name: string;
-  score: number;
-}
-
-interface GameVariables {
-  movieArray: MovieDoc[];
-  correctMovieName: string;
-  characterArray: Doc[];
-  correctCharacterName: string;
-  userMovieAnswer: string;
-  userCharacterAnswer: string;
-  gameCounter: number;
-  score: number;
-  randomNumberMovie: number;
-  randomNumberCharacter: number;
-  moviePhotoArray: any[];
-  characterPhotoArray: any[];
-  gameType: string;
-  headerTitle: string;
-  userCorrectFeedback: {
-    rightMovie: number;
-    rightCharacter: number;
-  };
-  qHighscores: Highscores[];
-  sHighscores: Highscores[];
-}
 
 let gameData: GameVariables = {
   movieArray: [],
---
+++ quiz_server/types.ts
@@ -0,0 +1,150 @@
+import { ObjectId } from "mongodb";
+
+// Player interface which is saved in databank
+export interface Player {
+    name: string;
+    ww: string;
+    qscore?: number;
+    sdscore?: number;
+    favorites?: FavoriteList[];
+    blacklisted?: Blacklist[];
+}
+
+// Blacklist interface
+export interface Blacklist {
+    characterName: string;
+    blacklistQuotes: string[];
+    reason: string[];
+}
+
+// FavoriteList interface
+export interface FavoriteList {
+    characterName: string;
+    favoriteQuotes: string[];
+    characterInfo: Doc;
+}
+
+//API character interface
+export interface Characters {
+    docs: Doc[];
+    total: number;
+    limit: number;
+    offset: number;
+    page: number;
+    pages: number;
+}
+
+export interface Doc {
+    _id: string;
+    height: string;
+    race: string;
+    gender?: Gender;
+    birth: string;
+    spouse: string;
+    death: string;
+    realm: string;
+    hair: string;
+    name: string;
+    wikiUrl?: string;
+}
+
+export enum Gender {
+    Empty = "",
+    Female = "Female",
+    GenderMale = "male",
+    Male = "Male",
+    Males = "Males",
+    MostLikelyMale = "Most likely male",
+    NaN = "NaN",
+}
+
+//API quote interfaces
+export interface Quotes {
+    docs: qDoc[];
+    total: number;
+    limit: number;
+    offset: number;
+    page: number;
+    pages: number;
+}
+
+export interface qDoc {
+    _id: string;
+    dialog: string;
+    movie: Movie;
+    character: string;
+    id: string;
+}
+
+export enum Movie {
+    The5Cd95395De30Eff6Ebccde5B = "5cd95395de30eff6ebccde5b",
+    The5Cd95395De30Eff6Ebccde5D = "5cd95395de30eff6ebccde5d",
+}
+
+//API Movie interface
+export interface Movies {
+    docs: MovieDoc[];
+    total: number;
+    limit: number;
+    offset: number;
+    page: number;
+    pages: number;
+}
+
+export interface MovieDoc {
+    _id: string;
+    name: string;
+    runtimeInMinutes: number;
+    budgetInMillions: number;
+    boxOfficeRevenueInMillions: number;
+    academyAwardNominations: number;
+    academyAwardWins: number;
+    rottenTomatoesScore: number;
+}
+
+// Random generator interface
+export interface RandomFunction {
+    (min: number, max: number): number;
+}
+
+export interface CombineArrayElements {
+    (arrayEmpty: any[], arrayToTransfer: any[]): void;
+}
+
+// interface to save data from the API
+export interface ApiData {
+    quote: qDoc;
+    charactersArray: Doc[];
+    moviesArray: MovieDoc[];
+    correctCharacterName: string;
+    correctMovieName: string;
+}
+
+// main game variables used in the quiz
+export interface Highscores {
+    name: string;
+    score: number;
+}
+
+export interface GameVariables {
+    movieArray: MovieDoc[];
+    correctMovieName: string;
+    characterArray: Doc[];
+    correctCharacterName: string;
+    userMovieAnswer: string;
+    userCharacterAnswer: string;
+    gameCounter: number;
+    score: number;
+    randomNumberMovie: number;
+    randomNumberCharacter: number;
+    moviePhotoArray: any[];
+    characterPhotoArray: any[];
+    gameType: string;
+    headerTitle: string;
+    userCorrectFeedback: {
+        rightMovie: number;
+        rightCharacter: number;
+    };
+    qHighscores: Highscores[];
+    sHighscores: Highscores[];
+}
