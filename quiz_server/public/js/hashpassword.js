5ded09bc6a0ab7aa31483edb03e1dabed43f628a
Joeri Roosen <joeri.roosen@ap.be>
2023-05-15 21:00:18 +0200

    Slightly adjusted the hashpassword.js for a bugfix

--- quiz_server/public/js/hashpassword.js
+++ quiz_server/public/js/hashpassword.js
@@ -1,15 +1,4 @@
-// function mySubmit(obj) {
-//     let pwdObj = document.getElementsByClassName('pwd');
-//     let hashObj = new jsSHA("SHA-512", "TEXT", {numRounds: 1});
-//     for (const iterator of pwdObj) {
-//       hashObj.update(iterator.value);
-//       let hash = hashObj.getHash("HEX");
-//       iterator.value = hash;
-//     }
-
-//   }
-
-  function mySubmit(obj) {
+ function mySubmit(obj) {
     let pwdObj = document.getElementsByClassName('pwd');
     
     for (const iterator of pwdObj) {
